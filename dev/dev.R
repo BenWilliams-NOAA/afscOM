ab62f67cd46cdf3972f92a8d671b4d95c9bd630e
Ben.Williams <ben.williams@noaa.gov>
2024-01-26 11:51:53 -0900

    add dev

--- dev/dev.R
+++ dev/dev.R
@@ -4,8 +4,9 @@
 library(devtools)
 library(pkgdown)
 
-
+# ignore the development folder from package building
 usethis::use_build_ignore("dev")
+
 # dev code
 use_package('dplyr')
 use_package('TMB')
@@ -13,4 +14,13 @@ use_package('TMB')
 use_mit_license()
 
 # add functions
-use_r('fish_age_comp')
+# use_r('fish_age_comp')
+# use_r('slx_a50_a95')
+
+# create the package
+devtools::document()
+devtools::build()
+
+# create website
+# this will create a 'docs' folder where vignettes etc for the associated github website will be held
+pkgdown::build_site(examples = FALSE)
