c1bb352f48e3dec6a5b2b7f6e9fd825afc8be02e
Joshua Zahner <joshuazahner@gmail.com>
2024-03-08 17:03:30 -0800

    fix: Require zaa input to calculate survey RPN and RPW indices
    Added required zaa parameter to simulate_rpn and simulate_rpw to correctly calcualte mid-year survey indices

--- R/obs_utils.R
+++ R/obs_utils.R
@@ -1,4 +1,38 @@
-simulate_rpn <- function(q, naa, sel){
-    total_naa <- apply(naa, 2, sum)
-    return(q*sum(naa*sel))
+#' Simulate Relative Population Numbers Index
+#' 
+#' Generate a relative population number index based on
+#' a survey occurring halfway through the year.
+#'
+#' @param q catchability coefficient
+#' @param naa numbers-at-age vector
+#' @param sel selectivity-at-age vector
+#' @param zaa total mortality-at-age vector
+#'
+#' @export simulate_rpn
+#'
+#' @example
+#'
+simulate_rpn <- function(q, naa, sel, zaa){
+    #total_naa <- apply(naa, 2, sum)
+    return(q*sum(naa*exp(-zaa/2)*sel))
+}
+
+#' Simulate Relative Population Weight Index
+#' 
+#' Generate a relative population weight index based on
+#' a survey occurring halfway through the year.
+#'
+#' @param q catchability coefficient
+#' @param naa numbers-at-age vector
+#' @param waa weight-at-age vector
+#' @param sel selectivity-at-age vector
+#' @param zaa total mortality-at-age vector
+#'
+#' @export simulate_rpw
+#'
+#' @example
+#'
+simulate_rpw <- function(q, naa, waa, sel, zaa){
+    #total_naa <- apply(naa, 2, sum)
+    return(q*sum(naa*exp(-zaa/2)*sel*waa))
 }
