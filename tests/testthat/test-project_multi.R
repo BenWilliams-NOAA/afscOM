f1891a15c3c807171d41c2f820ba8a47a052933a
Joshua Zahner <joshuazahner@gmail.com>
2024-07-26 03:00:58 -0700

    test: Update project_multi test

--- tests/testthat/test-project_multi.R
+++ tests/testthat/test-project_multi.R
@@ -13,7 +13,7 @@ test_that("project_multi reproduces sablefish timeseries", {
     init_naa[,,2,] <- assessment$natage.male["1960",]
 
     recruitment <- assessment$natage.female[,1]*2
-    recruitment <- c(recruitment, recruitment[64])
+    recruitment <- as.matrix(c(recruitment, recruitment[64]))
 
     f_timeseries <- assessment$t.series[,c("F_HAL", "F_TWL")] %>% as.matrix
     f_timeseries <- array(f_timeseries, dim=c(dims$nyears, 1, 1, 1, 2), 
@@ -25,10 +25,19 @@ test_that("project_multi reproduces sablefish timeseries", {
 
     model_options <- list(
         removals_input = "F",
-        simulate_observations = FALSE
+        simulate_observations = FALSE,
+        recruit_apportionment = NULL,
+        recruit_apportionment_random = FALSE
     )
 
-    om_sim <- project_multi(init_naa, f_timeseries, recruitment, dem_params, nyears, model_options)
+    om_sim <- project_multi(
+        init_naa, 
+        f_timeseries, 
+        recruitment, 
+        dem_params, 
+        nyears, 
+        model_options
+    )
 
     om_ssb <- as.vector(apply(om_sim$naa[1:64,,1,]*dem_params$waa[,,1,]*dem_params$mat[,,1,], 1, sum))
     true_ssb <- assessment$t.series[,"spbiom"]
