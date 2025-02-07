d44f45e775cf962da1f275374f55bf571493ba5a
Joshua Zahner <joshuazahner@gmail.com>
2025-02-07 14:22:00 -0800

    fix: Add region dimension to sizeage matrix dimensions

--- R/lengths.R
+++ R/lengths.R
@@ -4,16 +4,17 @@ compute_length_structure <- function(naa, sizeage_matrix){
     nlengths <- model_dims[1]
     nages <- model_dims[2]
     nsexes <- model_dims[3]
-    nyears <- model_dims[4]
+    nregions <- model_dims[4]
+    nyears <- model_dims[5]
     
     v <- vapply(
         1:nyears,
         function(y){
-            sizeage <- sizeage_matrix[,,,y]
+            sizeage <- sizeage_matrix[,,,,y]
             sapply(
                 1:model_params$nsexes,
                 # Apply movement to the sexes individually
-                function(s) naa[y,,s,] %*% sizeage_matrix[,,s,y]
+                function(s) naa[y,,s,] %*% sizeage_matrix[,,s,,y]
             )
         },
         FUN.VALUE = array(0, dim=c(nlengths, nsexes))
